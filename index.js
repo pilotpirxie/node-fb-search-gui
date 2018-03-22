7741db2dafa1e01a1b1a67d80588ff931ead23d6
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2018-03-22 23:44:23 +0100

    Installed more dependencies
    Express, cookie parser, body parser, look package json for more

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const hbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
 
-app.listen(3000, () => {
-    console.log('App is listening on port 3000!');
+// settings
+app.engine('hbs', hbs({extname: '.hbs'}));
+app.set('port', process.env.PORT || 80);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(flash());
+
+// routing
+app.use('/', require('./routes/index.js'));
+
+// server
+const server = app.listen(app.get('port'), () => {
+    console.log(`App is listening on port ${server.address().port}!`);
 });
