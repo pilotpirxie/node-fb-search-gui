bdf18eda4e7b4faf39a34813da4c2e728c960cb1
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2018-03-25 01:08:06 +0100

    Added method for retrieving data from graph

--- controllers/GraphController.js
+++ controllers/GraphController.js
@@ -1,6 +1,20 @@
-class GraphController {
-    constructor() {
+const request = require('request');
 
+class GraphController {
+    constructor(accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    retrieve(url) {
+        return new Promise((resolve, reject) => {
+            request.get(url + `?access_token=${this.accessToken}`, (err, res, body) => {
+                if ( err ) {
+                    reject(err);
+                } else {
+                    resolve(body);
+                }
+            });
+        });
     }
 }
 
---
+++ test/graph.js
@@ -0,0 +1,16 @@
+require('dotenv').config({path: '.env'});
+const chai = require('chai');
+const assert = chai.assert;
+const Graph = require('../controllers/GraphController');
+
+const fbgraph = new Graph(process.env.FB_ACCESS_TOKEN);
+
+it('request to graph', async () => {
+    return fbgraph.retrieve('https://graph.facebook.com/v2.9/1085903488198718/insights/page_impressions/day').then(body => {
+      assert.typeOf(body, 'string');
+      console.log(JSON.parse(body));
+  }).catch(err => {
+      assert.fail(err);
+  });
+
+});
