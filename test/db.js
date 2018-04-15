ca4500fc46ca86854732fad946981d8ee1db50ee
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2018-04-16 00:20:30 +0200

    Test of report insert method & added exit argument

--- test/db.js
+++ test/db.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const databaseConfig = require('../config/database.js');
 mongoose.connect(databaseConfig.mongourl);
 const User = require('../models/users');
+const Report = require('../models/reports');
 
 describe('User', function() {
     it('Add new user', () => {
@@ -15,6 +16,26 @@ describe('User', function() {
             shortAccessToken: 'asdasd',
         }).then(user => {
             console.log(user);
+            assert.typeOf(user, 'object');
+        }).catch(err => {
+            console.log(err);
+        });
+      });
+});
+
+describe('Report', function() {
+    it('Add new report', () => {
+        Report.create({
+            createDate: (new Date()),
+            keywords: 'test,keywords',
+            range: '3',
+            coordinates: '52.295042284537345;-6.328125',
+            params: []
+        }).then(report => {
+            console.log(report);
+            assert.typeOf(report, 'object');
+        }).catch(err => {
+            console.log(err);
         });
       });
 });
