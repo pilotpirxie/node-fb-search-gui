9b98de5475a6db0c7be3d652f24244f493239e8b
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2018-04-17 23:07:49 +0200

    Test for page model

--- test/db.test.js
+++ test/db.test.js
@@ -5,6 +5,7 @@ const databaseConfig = require('../config/database.js');
 mongoose.connect(databaseConfig.mongourl);
 const User = require('../models/users');
 const Report = require('../models/reports');
+const Page = require('../models/pages');
 
 describe('User', function() {
     it('Add new user', () => {
@@ -34,6 +35,29 @@ describe('Report', function() {
         }).then(report => {
             console.log(report);
             assert.typeOf(report, 'object');
+        }).catch(err => {
+            console.log(err);
+        });
+      });
+});
+
+describe('Page', function() {
+    it('Add new page', () => {
+        Page.create({
+            createDate: (new Date()),
+            reportID: '3453245345345-3245-3-45',
+            pageID: '12312312391231-312-3-123-1-23',
+            name: 'Test Restaurant',
+            category: 'Food',
+            cover: 'https://example.com/img/test.jpg',
+            description: 'Lorem ipsum dolor sit amet.',
+            website: 'https://example.com',
+            fanCount: Number.parseInt('43'),
+            talkingAbout: Number.parseInt('41'),
+            checking: Number.parseInt('12551')
+        }).then(page => {
+            console.log(page);
+            assert.typeOf(page, 'object');
         }).catch(err => {
             console.log(err);
         });
